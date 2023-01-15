91405e0796256c667e087c1938bb492aeb91f253
sojinshin <jane20180@naver.com>
2023-01-15 20:02:35 +0900

    [#1] 메인페이지 영역 구분

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './assets/css.css'
 import reportWebVitals from './reportWebVitals';
 import router from './router'
 import {RouterProvider} from "react-router-dom";
