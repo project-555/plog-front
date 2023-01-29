22864107e15ad08c7fed1989d8533e48f9893235
sojinshin <jane20180@naver.com>
2023-01-29 22:10:11 +0900

    [DP] #2 UMS API 요청 (로그인)

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,20 +1,56 @@
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 import {Button, IconButton} from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
-import logo from '../../assets/static/logo.png';
+// import logo from '../../assets/static/logo.png';
 
+
+interface userType {
+    "sub": string;
+    "roles": string[];
+    "userID": number;
+    "nickname": string;
+    "blogID": number;
+    "iat": number;
+    "exp": number;
+
+}
 export default function Header (){
+
+    const [userInfo, setUserInfo] = useState<userType | object>({});
+
+    useEffect(()=>{
+        const token = localStorage.getItem('token')
+        const decode = jwt_decode(token || '');
+        setUserInfo(decode || {})
+        // console.log(decode)
+
+    }, [])
+
+    console.log(userInfo)
 
     return (
         <div className='header' >
-            <div><a href='/'><img src={logo} alt='logo' className='logo'/></a></div>
+            {/*<div><a href='/'><img src={logo} alt='logo' className='logo'/></a></div>*/}
+            <div><a href='/'><span className='logo'>flog</span></a></div>
             <div>
                 <NavLink to='/search'>
                     <IconButton  aria-label="search" disabled color="primary">
                         <SearchIcon className='search-btn'/>
                     </IconButton>
                 </NavLink>
-                <NavLink to='/sign-in'><Button className='login-btn' variant="contained">로그인</Button></NavLink>
+                {
+                    true ?
+                        <NavLink to='/sign-in'>
+                            <Button className='login-btn' variant="contained">로그인</Button>
+                        </NavLink>
+                        :
+                        <NavLink to='/sign-in'>
+                            <Button className='login-btn' variant="contained">유저이름</Button>
+                        </NavLink>
+                }
+
             </div>
         </div>
     )
--- src/pages/signin/SignIn.tsx
+++ src/pages/signin/SignIn.tsx
@@ -1,25 +1,46 @@
-import { useState } from "react";
-import {useNavigate} from "react-router-dom";
-import Button from "@mui/material/Button";
-import logo from '../../assets/static/logo_login.png'
+import {useState} from 'react'
+import {useNavigate} from 'react-router-dom';
+import axios from 'axios'
+import logo from '../../assets/static/logo_pic.png';
 
 export function SignIn (){
     const navigate = useNavigate();
+    const [email, setEmail] = useState<string>('beyonce@rirively.com');
+    const [password, setPassword] = useState<string>('rirively');
+
+    const loginRequest = () => {
+        const params = {
+            "email": email,
+            "password": password
+        }
+
+        axios.post('http://api.plogcareers.com/auth/login', params)
+            .then((res:any) => {
+                if(res.status === 200){
+                    const token = res.data.data.token.accessToken
+                    // console.log(token)
+                    localStorage.setItem('token', token)
+                    navigate('/')
+                }
+                else { console.log(res.status)}
+            })
+    }
 
     return (
-            <div className="login-container">
-                <div className="login-modal">
-                    <form className="login-form">
-                        <img src={logo} className="login-logo" alt='logo_login'/>
-                        <p className="login-title">로그인</p>
-                        <div className="login-inputs">
-                            <input className= "login-input" type="email" placeholder='email'/>
+            <div className='login-container'>
+                <div className='login-modal'>
+                    <div className='login-form'>
+                        <a href='/'><img src={logo} className="login-logo" alt='logo_login'/></a>
+                        <p className='login-title'>로그인</p>
+                        <div className='login-inputs'>
+                            <input className= 'login-input' type='email' placeholder='email' onChange={(e)=>setEmail(e.target.value)}/>
                         </div>
-                        <div className="login-inputs">
-                            <input className="login-input" type="password" placeholder='password'/>
+                        <div className='login-inputs'>
+                            <input className='login-input' type='password' placeholder='password' onChange={(e)=>setPassword(e.target.value)}/>
                         </div>
-                        <button className="login-btn" onClick={()=>{navigate('/')}}>Login</button>
-                    </form>
+                        <button className='login-btn' onClick={loginRequest}>Login</button>
+                        <p className='sentence'>아직 회원이 아니신가요? <span className='sign-up' onClick={()=>navigate('/sign-up')}>회원가입</span></p>
+                    </div>
                 </div>
             </div>
     )
