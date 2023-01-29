f2422b73d4a4a4e8ef5fbb23312f776fcb8428bc
sojinshin <jane20180@naver.com>
2023-01-29 22:11:59 +0900

    [DP] 포스팅 썸네일, 상세페이지 목업 데이터 추가 및 디자인 변경

--- src/components/blog/PostCard.tsx
+++ src/components/blog/PostCard.tsx
@@ -1,44 +1,52 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import {Card,CardHeader,CardMedia,CardContent,CardActions, Avatar, Typography, IconButton} from '@mui/material';
-// import {red} from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 import '../../assets/postcard.css'
 
+interface postType {
+    "categoryID": number;
+    "createDt": string;
+    "hitCnt": number;
+    "htmlContent": string;
+    "id": number;
+    "isCommentAllowed": boolean;
+    "isStarAllowed": boolean;
+    "mdContent": string;
+    "postingStarCount": number;
+    "stateID": number;
+    "thumbnailImageUrl":string;
+    "title": string;
+    "updateDt": string;
+}
 
-export default function PostCard() {
+
+type ChildProps  = {
+    postInfo : postType
+}
+
+export default function PostCard({postInfo}: ChildProps ) {
     const navigate = useNavigate();
 
     return (
-        <Card className='posting-container' sx={{maxWidth: 345, display: 'inline-block'}} >
+        <Card sx={{maxWidth: 305, display: 'inline-block',margin: '3px'}} >
             <CardHeader
                 avatar={
                     <Avatar  aria-label="recipe">
                         R
                     </Avatar>
                 }
-                action={
-                    <IconButton aria-label="settings">
-                        <MoreVertIcon/>
-                    </IconButton>
-                }
-                title="Shrimp and Chorizo Paella"
-                subheader="September 14, 2016"
+                title={`by ${postInfo.categoryID}`}
+                subheader={postInfo.createDt}
             />
             <CardMedia
-                component="img"
-                height="194"
-                image="https://mblogthumb-phinf.pstatic.net/MjAyMDAxMDVfMjc4/MDAxNTc4MjI5NjIzMDM2.6O6yTIzzfBeLuNylFDt6JjG4iHHQpLBxEHpNZeBAbmEg.ZrWMQUWSBcm7hekDR4-NryoaGfZRsLI5xrlC3U5OOTQg.JPEG.sunmyung47/IMG_5988.jpg?type=w800"
-                alt="Paella dish"
-                onClick={()=>{navigate('/blogs/:blogID/postings/:postingID')}}
-            />
-            <CardContent>
-                <Typography className='posting-title' variant="body1" color="text.first">
-                    This impressive paella is a perfect party
+                component="img" height="150" image={postInfo.thumbnailImageUrl} alt="썸네일 사진"
+                onClick={()=>{navigate('/blogs/:blogID/postings/:postingID')}}/>
+            <CardContent className='postcard-contents' sx={{paddingBottom:0,}}>
+                <Typography className='postcard-title' variant="body1" color="text.first">
+                    {postInfo.title}
                 </Typography>
-                <Typography className='posting-summary' variant="body2" color="text.secondary">
+                <Typography className='postcard-summary' variant="body2" color="text.secondary">
                     This impressive paella is a perfect party dish and a fun meal to cook
                     together with your guests. Add 1 cup of frozen peas along with the mussels,
                     if you like.
@@ -47,9 +55,6 @@ export default function PostCard() {
             <CardActions disableSpacing>
                 <IconButton aria-label="add to favorites">
                     <FavoriteIcon/>
-                </IconButton>
-                <IconButton aria-label="share">
-                    <ShareIcon/>
                 </IconButton>
             </CardActions>
         </Card>
--- src/pages/posting/PostingDetail.tsx
+++ src/pages/posting/PostingDetail.tsx
@@ -1,15 +1,15 @@
-import Header from '../../components/common/Header';
-
 export function PostingDetail (){
     return (
         <>
-            <Header/>
-            <div className='postingTitleArea' style={{borderBottom:'1px solid #ddd', height:'200px', textAlign:'center'}}>
-                <p>포스팅 제목을 나타낸다.</p>
-                <p>작성자와 날짜 그리고 태그들을 나타낸다.</p>
+            <div className='posting-header-area' style={{borderBottom:'1px solid #ddd', height:'200px', textAlign:'center'}}>
+                <h1 className='title'>포스팅 제목 - 부제</h1>
+                <div className='posting-info'>
+                    <span className='bolder'>user id</span>
+                    <span className='explain'> 10분전</span>
+                </div>
             </div>
-            <div className='postingContentsArea' style={{textAlign:'center'}}>
-                <div style={{backgroundColor:'#eee', height:'1200px', width:'900px', margin:'auto'}}>포스팅 내용들</div>
+            <div className='posting-contents-area'>
+                <p>포스팅 내용들</p>
             </div>
         </>
 
