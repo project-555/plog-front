b1951780a8418ad9ddcf569724aba6cfa2d1bf0d
sojinshin <jane20180@naver.com>
2023-01-29 22:08:50 +0900

    [DP] 디렉토리 변경 및 로고 이미지 파일 추가

---
+++ src/pages/main/SearchPosting.tsx
@@ -0,0 +1,10 @@
+export function SearchPosting(){
+    return (
+        <div className='search-container'>
+            <div className="search-description">포스트 검색</div>
+            <div className="input-wrapper">
+                <input className='search-input' placeholder="검색어를 입력하세요"/>
+            </div>
+        </div>
+    )
+}
