9653825a445c9ccb9f2bf057092278468e914aa1
sojinshin <jane20180@naver.com>
2023-02-22 16:56:08 +0900

    [DP] #3 import UMS type

--- src/pages/signin/SignIn.tsx
+++ src/pages/signin/SignIn.tsx
@@ -2,7 +2,7 @@ import {useState} from 'react'
 import {useNavigate} from 'react-router-dom';
 import axios from 'axios'
 import logo from '../../assets/static/logo_pic.png';
-import {Login} from '../../types/userType';
+import {Login} from '../../types/UMSType';
 
 export function SignIn (){
     const navigate = useNavigate();
