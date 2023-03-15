ccda44bc448958bc555a4aa3c1a89a1caa09947c
sojinshin <jane20180@naver.com>
2023-03-15 20:58:08 +0900

    [DP] 라우터 정리

--- src/Router.tsx
+++ src/Router.tsx
@@ -13,11 +13,12 @@ import {ForgotPassword} from "./pages/signin/ForgotPassword";
 const router = createBrowserRouter(
     createRoutesFromElements(
 <Route path="/" element={<Root />}>
-    <Route path="main" element={<BlogMain/>} />  {/*Page When User want to login plog*/}
+    <Route path="/" element={<BlogMain/>} />  {/*Page When User want to login plog*/}
+    {/*<Route path="main" element={<BlogMain/>} />  /!*Page When User want to login plog*!/*/}
     <Route path="sign-in" element={<SignIn/>} />  {/*Page When User want to login plog*/}
     <Route path="sign-up" element={<SignUp/>} />  {/*Page When User ant to join plog*/}
     <Route path="forgot-password" element={<ForgotPassword/>} />  {/*Page When User forgot password page*/}
-    <Route path="blogs" element={<BlogMain/>} />  {/*Page blog domain main*/}
+    {/*<Route path="blogs" element={<BlogMain/>} />  /!*Page blog domain main*!/*/}
     <Route path="blogs/:blogID" element={<UserBlog/>} />  {/*Page plog user's blog home*/}
     <Route path="blogs/:blogID/write-posting" element={<PostingWrite/>} />   {/*Page when user want to write posting*/}
     <Route path="blogs/:blogID/postings/:postingID" element={<PostingDetail/>} />  {/*Blog posting detail page*/}
