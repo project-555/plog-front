0627da23801f44113911e8f82ab40ade311554e0
sojinshin <jane20180@naver.com>
2023-09-17 22:02:09 +0900

    [#29] 포스팅 설정값에 따른 별/댓글 권한 제어 추가, 디테일 페이지 css수정

--- src/pages/posting/PostingDetail.tsx
+++ src/pages/posting/PostingDetail.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
-import {useParams, useLocation} from 'react-router-dom'
-import {plogAxios} from "../../modules/axios";
+import {useParams, useNavigate} from 'react-router-dom'
+import {plogAuthAxios, plogAxios} from "../../modules/axios";
 import { Viewer } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor-viewer.css';
 import Comment from '../../components/blog/Comment'
@@ -9,10 +9,22 @@ import Toc from "../../components/blog/Toc";
 
 export function PostingDetail (){
 
-    const location = useLocation()
-    const nickname = location.state.nickname;
+    const navigate = useNavigate()
+
+    const token = localStorage.getItem('token')
+    const userID = localStorage.getItem('userID')
     const {blogID, postingID}  = useParams();
+    const [nickname, setNickname] = useState('')
     const [post, setPost] = useState(null);
+
+    useEffect(()=> {
+        if(!!token && !!userID){
+            plogAxios.get(`/blogs/${blogID}`)
+                .then(response => {
+                    setNickname(response.data.data.blogUser.nickname)
+                })
+        }
+    },[])
 
     useEffect(()=> {
         plogAxios.get(`/blogs/${blogID}/postings/${postingID}`)
@@ -22,25 +34,48 @@ export function PostingDetail (){
             .catch(err => console.log(err.message))
     }, [])
 
+
+    const delThisPosting = () => {
+        const result = window.confirm('해당 포스팅을 삭제하시겠습니까?')
+
+        if(result){
+            plogAuthAxios.delete(`blogs/${blogID}/postings/${postingID}`)
+                .then(() => {
+                    alert('포스팅을 삭제했습니다')
+                    navigate('/')
+                    }
+                )
+        }else {
+
+        }
+    }
+
     return (
         <div  className='inner-container'>
             {
                 post &&
                 <>
                     <div style={{display:'flex', justifyContent:'space-evenly',alignItems: 'flex-start', minHeight:'1200px' }} >
-                        <StarShare blogId={blogID!} postingId={postingID!}/>
-                        <div className='content-container'>
+                        {post['isStarAllowed'] && <StarShare blogId={blogID!} postingId={postingID!}/>}
+                        <div className='content-container'  style={{flexGrow:'1'}}>
                             <div className='posting-header-area' style={{height:'200px', textAlign:'center'}}>
                                 <h1 className='title'>{post['title']}</h1>
                                 <div className='posting-info'>
-                                    <span className='bolder'>{nickname}</span>
-                                    <span className='explain'>{post['updateDt']}</span>
+                                    <div>
+                                        <span className='bolder'>{nickname}</span>
+                                        <span className='explain'>{post['updateDt']}</span>
+                                    </div>
+                                    <ul>
+                                        <li onClick={()=>navigate(`/blogs/${blogID}/postings/${postingID}/edit-posting`)}>수정</li>
+                                        <li onClick={delThisPosting}>삭제</li>
+                                    </ul>
+
                                 </div>
                             </div>
                             <div className='posting-contents-area'>
                                 <Viewer initialValue={post['htmlContent']}/>
                             </div>
-                            <Comment/>
+                            <Comment isCommentAllowed={post['isCommentAllowed']}/>
                         </div>
                         <Toc htmlString={post['htmlContent']}/>
                     </div>
