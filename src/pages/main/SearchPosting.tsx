6d355eb3b824188ab344bc97bf022639c2fefb5c
sojinshin <jane20180@naver.com>
2023-10-04 18:43:02 +0900

    [#41] 검색페이지 및 메인 페이지 유저 프로필 아바타 -> 이미지로 수정

--- src/pages/main/SearchPosting.tsx
+++ src/pages/main/SearchPosting.tsx
@@ -1,9 +1,12 @@
-import {useState} from 'react';
+import React, {useState} from 'react';
+import {useNavigate}  from "react-router-dom";
 import {plogAxios} from "../../modules/axios";
 import {postType} from "../../types/PostingType";
+import {Avatar, CardHeader} from "@mui/material";
 
 export function SearchPosting(){
 
+    const navigate = useNavigate();
     const [resultDesc, setResultDesc] = useState(false)
     const [keyword, setKeyword] = useState('');
     const [filteredPosting, setFilteredPosting] = useState([])
@@ -31,6 +34,10 @@ export function SearchPosting(){
         return `${year}년 ${month}월 ${day}일`
     }
     // @ts-ignore
+
+    const moveToPost = (blogID, postingID) => {
+        navigate(`/blogs/${blogID}/postings/${postingID}`)
+    }
     return (
         <div className='search-container'>
             <div className="search-description">포스트 검색</div>
@@ -47,25 +54,36 @@ export function SearchPosting(){
             {filteredPosting.length > 0 && filteredPosting.map((post: any) =>
                 <div className="sc-hOGkXu fqIhgM" style={{marginBottom:'4rem'}}>
                     <div className="user-info" style={{display: 'flex',alignItems: 'center', marginBottom: '1.5rem'}}>
-                        <img
-                             style={{marginRight:'0.5rem', width:'30px', height:'30px', borderRadius:'50%'}}
-                             src={post.homePostingUser.profileImageURL} alt="thumbnail"/>
+                        {!!post.homePostingUser.profileImageURL ?
+                            <CardHeader
+                                avatar={<img style={{width:'40px', height:'40px', borderRadius:'50%'}} src={post.homePostingUser.profileImageURL} alt="thumbnail"/>}
+                                style={{padding: 0}}
+                            />
+                            :
+                            <CardHeader
+                                avatar={<Avatar  aria-label="recipe">{post.homePostingUser.nickname[0]}</Avatar>}
+                                style={{padding: 0}}
+                            />
+                        }
                         <div className="username">
                             {post.homePostingUser.nickname}
                         </div>
                     </div>
 
-                    <div className="post-thumbnail" style={{marginBottom: '1rem'}} >
-                        {post.thumbnailImageURL &&
-                        <img style={{width:'100%',height:'402px', objectFit:'cover'}} src={post.thumbnailImageURL} alt="post-thumbnail"/>}
+                    <div onClick={()=> moveToPost(post.blogID, post.postingID)}>
+                        <div className="post-thumbnail" style={{marginBottom: '1rem'}} >
+                            {post.thumbnailImageURL &&
+                                <img style={{width:'100%',height:'402px', objectFit:'cover'}} src={post.thumbnailImageURL} alt="post-thumbnail"/>}
+                        </div>
+                        <h2 style={{wordBreak:'keep-all'}}>{post.title}</h2>
+                        <p style={{marginBottom: '2rem', marginTop: '0.5rem'}}>{post.summary}</p>
+                        <div className="subinfo" style={{color: '#868E96',display: 'flex', alignItems: 'center', marginTop: '1rem', fontSize: '0.875rem'}}>
+                            <span>{dateParser(post.createDt)}</span>
+                            <div className="separator" style={{margin: '0 0.5rem'}}>·</div>
+                            <span>{post.starCnt}개의 스타</span>
+                        </div>
                     </div>
-                    <h2 style={{wordBreak:'keep-all'}}>{post.title}</h2>
-                    <p style={{marginBottom: '2rem', marginTop: '0.5rem'}}>{post.summary}</p>
-                    <div className="subinfo" style={{color: '#868E96',display: 'flex', alignItems: 'center', marginTop: '1rem', fontSize: '0.875rem'}}>
-                        <span>{dateParser(post.createDt)}</span>
-                        <div className="separator" style={{margin: '0 0.5rem'}}>·</div>
-                        <span>{post.starCnt}개의 스타</span>
-                    </div>
+
                 </div>
 
             )}
