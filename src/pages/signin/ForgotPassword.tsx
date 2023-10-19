be6dc59f315e8c85394a41b6de7c03545fec52fd
sojinshin <jane20180@naver.com>
2023-10-19 21:42:47 +0900

    [#54] 계정 관련 페이지 다크모드 적용

--- src/pages/signin/ForgotPassword.tsx
+++ src/pages/signin/ForgotPassword.tsx
@@ -1,7 +1,8 @@
 import {useState} from "react";
 import {Box, Button, Grid, TextField, Typography} from '@mui/material';
 import {CodeParams} from "../../types/UMSType";
-import axios from "axios";
+import {getPlogAxios} from "../../modules/axios";
+
 
 
 export function ForgotPassword() {
@@ -16,7 +17,7 @@ export function ForgotPassword() {
         const params = {
             "email": account
         }
-        axios.post(`http://api.plogcareers.com/auth/send-verify-find-password-email`, params)
+        getPlogAxios().post(`/auth/send-verify-find-password-email`, params)
             .then(res => {
                 if (res.status === 200 || res.status === 204) {
                     setSendEmail(true)
@@ -33,7 +34,7 @@ export function ForgotPassword() {
             "verifyCode": verifyCode
         }
         if (verifyCode.length === 6 || e.keyCode === 9 || e.keyCode === 13) {
-            axios.post('http://api.plogcareers.com/auth/verify-find-password-email', params)
+            getPlogAxios().post('/auth/verify-find-password-email', params)
                 .then(res => {
                     if (res.status === 200) {
                         const token = res.data.verifyToken //인증번호 일치하면 토큰획득
@@ -56,36 +57,45 @@ export function ForgotPassword() {
             "password": password,
             "verifyToken": verifyToken
         }
-        axios.post('http://api.plogcareers.com/auth/change-password', params)
+        getPlogAxios().post('/auth/change-password', params)
             .then(res => alert(res.status))
             .catch(err => alert(err.message))
     }
 
     return (
 
-        <div className='signup-container'>
-            <div className='signup-modal'>
-                <Typography variant="h3" gutterBottom align="center">
-                    Forgot Password
-                </Typography>
-
-
-                <Box className='step1-container' sx={{mt: 6}}>
+        <div className='forgot-pw-container'>
+            <div className='forgot-pw-modal'>
+                <Typography variant="h3" gutterBottom align="center">Forgot Password</Typography>
+                <Box className='password-form-container' sx={{mt: 6}}>
                     <TextField className='email-input' variant="standard" label="이메일" name="이메일"
                                autoComplete="email" margin="normal"
                                onChange={(e) => setAccount(e.target.value)}
-                        // helperText={regCheck(account, 'email') ? '' : '올바른 이메일 형식을 입력해주세요'}
+                               sx={{
+                                   '& input.MuiInputBase-input':{color: 'var(--text1)', borderBottom: '1px solid var(--form-border)'},
+                                   '& label.MuiFormLabel-root':{color: 'var(--text1)'},
+                               }}
                                required/>
-                    <Button className='email-chk' size="small" color='success'
-                        // disabled={regCheck(account, 'email') === false || account === ''}
+                    <Button className='email-chk' size="small"
                             variant={sendEmail ? 'contained' : 'outlined'}
-                            onClick={sendEmailRequest}>
+                            sx={{
+                                backgroundColor:sendEmail ? 'var(--primary1)' : '',
+                                color:sendEmail?'#fff':'var(--border)',
+                                border: sendEmail?'none':'1px solid var(--border)',
+                            }}
+                            onClick={sendEmailRequest}
+                    >
                         인증코드 전송
                     </Button>
                     <Grid container spacing={2} style={{display: sendEmail ? 'block' : 'none'}}>
                         <Grid item xs={12} sm={6}>
                             <TextField variant="standard" autoFocus autoComplete="given-name" label="인증번호" name="인증번호"
                                        inputProps={{maxLength: 6,}}
+                                       sx={{
+                                           '& .MuiFormHelperText-root':{color:'var(--error)'},
+                                           '& input.MuiInputBase-input':{color: 'var(--text1)', borderBottom: '1px solid var(--form-border)'},
+                                           '& label.MuiFormLabel-root':{color: 'var(--text1)'},
+                                       }}
                                        onChange={(e) => setVerifyCode(e.target.value)}
                                        onKeyDown={codeCheckRequest}
                                        helperText={!!verifyToken || verifyCode.length === 0 ? '' : '인증번호가 일치하지 않습니다.'}
@@ -99,11 +109,17 @@ export function ForgotPassword() {
                         variant="standard" fullWidth name="password" autoComplete="new-password" label="비밀번호"
                         type="password" margin="normal"
                         onChange={(e) => setPassword(e.target.value)}
+                        sx={{
+                            '& input.MuiInputBase-input':{color: 'var(--text1)', borderBottom: '1px solid var(--form-border)'},
+                            '& label.MuiFormLabel-root':{color: 'var(--text1)'},
+                        }}
                         required
                     />
 
 
-                    <Button variant='contained' className='signup-btn-full' onClick={changePassword}>
+                    <Button variant='contained'
+                            sx={{mt:1.5,color:'#fff',fontSize:'15px', width:'100%', height:"50px", backgroundColor:'var(--primary1)'}}
+                            onClick={changePassword}>
                         비밀번호 변경하기
                     </Button>
                 </Box>
