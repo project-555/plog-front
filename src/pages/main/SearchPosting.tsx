bd01421abfd3a99e0b6589a81fc7c01d382e81e2
sojinshin <jane20180@naver.com>
2023-10-30 18:20:08 +0900

    [#61,#64] 검색페이지 유저 프로필 없을 때 기본 이미지, 포스팅 요약 내용 추가

--- src/pages/main/SearchPosting.tsx
+++ src/pages/main/SearchPosting.tsx
@@ -1,13 +1,13 @@
-import {useState} from 'react';
+import React, {useState} from 'react';
 import {plogAxios} from "../../modules/axios";
 import {postType} from "../../types/PostingType";
+import {Avatar, CardHeader} from "@mui/material";
 
 export function SearchPosting(){
 
     const [resultDesc, setResultDesc] = useState(false)
     const [keyword, setKeyword] = useState('');
     const [filteredPosting, setFilteredPosting] = useState([])
-    const [tags, setTags] = useState([])
 
     const keywordChange = (e: any) => {
         setKeyword(e.target.value)
@@ -30,6 +30,22 @@ export function SearchPosting(){
         const day = date.substring(8,10)
         return `${year}년 ${month}월 ${day}일`
     }
+
+    const summary = (htmlstring: any) => {
+
+        const domParser = new DOMParser()
+        const htmlDoc = domParser.parseFromString(htmlstring, 'text/xml')
+        const bodyTagChildren = htmlDoc.getElementsByTagName('body')[0].childNodes
+
+        const text : any[]= []
+
+        bodyTagChildren.forEach(node => {
+            const contents = node.textContent
+            text.push(contents)
+        })
+
+        return text.join('\n').substring(0, 500)
+    }
     // @ts-ignore
     return (
         <div className='search-container'>
@@ -47,9 +63,11 @@ export function SearchPosting(){
             {filteredPosting.length > 0 && filteredPosting.map((post: any) =>
                 <div className="sc-hOGkXu fqIhgM" style={{marginBottom:'4rem'}}>
                     <div className="user-info" style={{display: 'flex',alignItems: 'center', marginBottom: '1.5rem'}}>
-                        <img
-                             style={{marginRight:'0.5rem', width:'30px', height:'30px', borderRadius:'50%'}}
-                             src={post.homePostingUser.profileImageURL} alt="thumbnail"/>
+                        {!!post.homePostingUser.profileImageURL ?
+                          <img style={{width:'30px', height:'30px', borderRadius:'50%', marginRight:'10px'}} src={post.homePostingUser.profileImageURL} alt="thumbnail"/>
+                            :
+                            <Avatar style={{width:'30px', height:'30px', borderRadius:'50%', marginRight:'10px'}} aria-label="recipe">{post.homePostingUser.nickname[0]}</Avatar>
+                        }
                         <div className="username">
                             {post.homePostingUser.nickname}
                         </div>
@@ -60,7 +78,9 @@ export function SearchPosting(){
                         <img style={{width:'100%',height:'402px', objectFit:'cover'}} src={post.thumbnailImageURL} alt="post-thumbnail"/>}
                     </div>
                     <h2 style={{wordBreak:'keep-all'}}>{post.title}</h2>
-                    <p style={{marginBottom: '2rem', marginTop: '0.5rem'}}>{post.summary}</p>
+                    <p id='postcard-summary'
+                        style={{marginBottom: '2rem', marginTop: '1rem', lineHeight:1.5, color:'var(--text3)', WebkitLineClamp: '6'}}>
+                        {summary(post.htmlContent)}</p>
                     <div className="subinfo" style={{color: '#868E96',display: 'flex', alignItems: 'center', marginTop: '1rem', fontSize: '0.875rem'}}>
                         <span>{dateParser(post.createDt)}</span>
                         <div className="separator" style={{margin: '0 0.5rem'}}>·</div>
